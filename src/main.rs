3623fb37e46555dd5d4a9fadddc6fb2ed8d85697
Earth Engine <earthengine@gmail.com>
2022-03-09 22:51:39 +1100

    Fix compile issue

--- src/main.rs
+++ src/main.rs
@@ -21,44 +21,10 @@ mod socks5;
 // mod stream_wrap;
 // mod tls_stream;
 
-use config::{OutgoingConfig, RocksConfig};
-use connection::bicopy;
-use incoming::{get_incoming, Incoming, IncomingClient};
-use outgoing::{get_outgoing, Outgoing, OutgoingError};
+use config::RocksConfig;
+use incoming::{get_incoming, Incoming};
 
-use crate::req_addr::ReqAddr;
-
-async fn abort(client: impl IncomingClient + Send, error: OutgoingError, req: ReqAddr) {
-    client
-        .abort(error, req)
-        .await
-        .unwrap_or_else(|e| error!("handle_request error: {}", e));
-}
-
-async fn process_request(mut client: impl IncomingClient + Send, o: impl Outgoing, r: ReqAddr) {
-    match o.process_request(r.clone()).await {
-        Ok(o) => match client.ready_for_connect(r).await {
-            Ok(i) => match bicopy(i, o).await {
-                Err(e) => error!("error during transfer: {}", e),
-                _ => (),
-            },
-            Err(e) => error!("can't get ready stream: {}", e),
-        },
-        Err(e) => abort(client, e, r).await,
-    }
-}
-
-async fn handle_client(mut client: impl IncomingClient + Send, outgoing_cfg: OutgoingConfig) {
-    match client.next_request().await {
-        Ok(r) => match get_outgoing(outgoing_cfg) {
-            Ok(o) => process_request(client, o, r).await,
-            Err(e) => abort(client, OutgoingError::GeneralFailure(e), r).await,
-        },
-        Err(e) => {
-            error!("can't handle request: {}", e)
-        }
-    }
-}
+use client_manager::handle_client;
 
 #[tokio::main]
 async fn main() -> Result<(), error::Error> {
@@ -99,8 +65,4 @@ async fn main() -> Result<(), error::Error> {
         }
     }
     Ok(())
-
-    /*
-    incoming.start(outgoing).await
-    */
 }
